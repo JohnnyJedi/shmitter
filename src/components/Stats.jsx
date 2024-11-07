45c71ac4a1fa20f6fff7e158ebc0714d067db728
Evgeny Pankratov <mr.johnny31.90@gmail.com>
2024-11-07 22:51:53 +0400

    First commit

---
+++ src/components/Stats.jsx
@@ -0,0 +1,25 @@
+import Avatar from "./Avatar.jsx";
+
+const Stats = ({user,stats}) => {
+    return (
+        <div className={'user-stats'}>
+
+            <div>
+                <Avatar user={user} alt={user}/>
+                {user.name}
+            </div>
+
+            <div className={'stats'}>
+                <div>
+                   Followers : {stats.followers}
+                </div>
+                <div>
+                    Following : {stats.following}
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Stats;
